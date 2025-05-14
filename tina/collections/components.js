c99d88b2cccb2cf643d20f35a8d15a41b60a4f53
Jose Alejandro Ovalle Villamil <125523373+jovalle02@users.noreply.github.com>
2025-05-14 12:27:22 -0500

    Added Tina Component

---
+++ tina/collections/components.js
@@ -0,0 +1,12 @@
+export default {
+  label: "Components",
+  name: "components",
+  path: "content/components",
+  format: "json",
+  fields: [
+    { name: "title", label: "Title", type: "string" },
+    { name: "subtitle", label: "Subtitle", type: "string" },
+    { name: "placeholder", label: "Placeholder", type: "string" },
+    { name: "buttonText", label: "Button Text", type: "string" },
+  ],
+};
